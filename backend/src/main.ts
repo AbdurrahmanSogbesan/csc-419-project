d7ec7432144b4faa19b5a996627df94d55ed6472
Abdulhaleem-6 <Abd6prif@gmail.com>
2025-03-01 01:47:00 +0100

    fix: dynamic port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new BigIntInterceptor());
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
